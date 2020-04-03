ac3c248930ec501e3921d0b2925cec86f2b6cd58
ke-sugiyama <sugiyama@sinfo-sys.co.jp>
2020-04-03 09:30:46 +0900

    初回コミット：4/2実施分

---
+++ src/stringKadai/StringKadai1.java
@@ -0,0 +1,24 @@
+package stringKadai;
+
+public class StringKadai1 {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・
+
+		String s1 = "東京都千代田区";
+		String s2 = "名古屋市中村区名駅";
+		String s3 = "大阪市";
+		System.out.println("s1 =" + s1);
+		System.out.println("s2 =" + s2);
+		System.out.println("s3 =" + s3);
+
+
+		int a4 = s1.length();
+		int a5 = s2.length();
+		int a6 = s3.length();
+		System.out.println("s1の文字列長は" + a4 + "文字です");
+		System.out.println("s2の文字列長は" + a5 + "文字です");
+		System.out.println("s3の文字列長は" + a6 + "文字です");
+
+	}
+
+}
